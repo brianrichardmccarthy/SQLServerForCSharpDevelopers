ed2deb3f31a0c9538ee7b5ec3ee675ed6ec3447c
Brian McCarthy <brianmccarthy1@outlook.ie>
2020-11-29 23:50:37 +0000

    feat: reset stock controller to basic crud

--- SQLServerForCSharpDevelopers.Controllers/Controllers/Products/V1/Stock/StockController.Operations.cs
+++ SQLServerForCSharpDevelopers.Controllers/Controllers/Products/V1/Stock/StockController.Operations.cs
@@ -4,7 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace SQLServerForCSharpDevelopers.Controllers.Controllers.Products.V1.Stock {
     public partial class StockController {
         [HttpPost("Create")]
-        public IActionResult Create([FromBody] Contracts.Products.Stock stock) {
+        public IActionResult CreateStock([FromBody] Contracts.Products.Stock stock) {
             return Ok();
         }
 
@@ -13,8 +13,8 @@ namespace SQLServerForCSharpDevelopers.Controllers.Controllers.Products.V1.Stock
             return Ok();
         }
 
-        [HttpPatch("Sale")]
-        public IActionResult Sale() {
+        [HttpPatch("Delete")]
+        public IActionResult DeleteStock() {
             return Ok();
         }
     }
--- SQLServerForCSharpDevelopers.Controllers/Controllers/Products/V1/Stock/StockController.Queries.cs
+++ SQLServerForCSharpDevelopers.Controllers/Controllers/Products/V1/Stock/StockController.Queries.cs
@@ -3,28 +3,8 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace SQLServerForCSharpDevelopers.Controllers.Controllers.Products.V1.Stock {
     public partial class StockController {
-        [HttpGet("{id}")]
-        public IActionResult GetBrand(Guid id) {
-            return Ok();
-        }
-
-        [HttpGet("StoreStock/{id}")]
-        public IActionResult GetStoreProductStock(Guid id) {
-            return Ok();
-        }
-
-        [HttpGet("ProductStock/{id}")]
-        public IActionResult GetProductStockInAllStores(Guid id) {
-            return Ok();
-        }
-
-        [HttpGet("GetStoreStock/{id}")]
-        public IActionResult GetStoreStock(Guid id) {
-            return Ok();
-        }
-
-        [HttpGet("ProductInStore/{productId}/{storeId}")]
-        public IActionResult ProductInStore(Guid productId, Guid storeId) {
+        [HttpGet("Read")]
+        public IActionResult Read() {
             return Ok();
         }
     }
