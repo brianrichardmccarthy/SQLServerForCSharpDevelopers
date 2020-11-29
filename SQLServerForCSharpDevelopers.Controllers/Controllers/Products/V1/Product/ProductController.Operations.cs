c635e0416112aeab542924142075339149c82e87
Brian McCarthy <brianmccarthy1@outlook.ie>
2020-11-29 23:52:48 +0000

    feat: reset products controller to basic crud

--- SQLServerForCSharpDevelopers.Controllers/Controllers/Products/V1/Product/ProductController.Operations.cs
+++ SQLServerForCSharpDevelopers.Controllers/Controllers/Products/V1/Product/ProductController.Operations.cs
@@ -1,4 +1,20 @@
-﻿namespace SQLServerForCSharpDevelopers.Controllers.Controllers.Products.V1.Product {
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SQLServerForCSharpDevelopers.Controllers.Controllers.Products.V1.Product {
     public partial class ProductController {
+        [HttpPost("Create")]
+        public IActionResult CreateProducts() {
+            return Ok();
+        }
+
+        [HttpPatch("Update")]
+        public IActionResult UpdateProducts() {
+            return Ok();
+        }
+
+        [HttpDelete("Delete")]
+        public IActionResult DeleteProducts() {
+            return Ok();
+        }
     }
 }
--- SQLServerForCSharpDevelopers.Controllers/Controllers/Products/V1/Product/ProductController.Queries.cs
+++ SQLServerForCSharpDevelopers.Controllers/Controllers/Products/V1/Product/ProductController.Queries.cs
@@ -1,4 +1,10 @@
-﻿namespace SQLServerForCSharpDevelopers.Controllers.Controllers.Products.V1.Product {
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SQLServerForCSharpDevelopers.Controllers.Controllers.Products.V1.Product {
     public partial class ProductController {
+        [HttpGet("Read")]
+        public IActionResult ReadProducts() {
+            return Ok();
+        }
     }
 }
