1775dee07cf3c59e2495ea158403b870e726c7e2
Brian McCarthy <brianmccarthy1@outlook.ie>
2020-11-29 23:49:00 +0000

    feat: reset category controller to basic crubd

--- SQLServerForCSharpDevelopers.Controllers/Controllers/Products/V1/Category/CategoryController.Queries.cs
+++ SQLServerForCSharpDevelopers.Controllers/Controllers/Products/V1/Category/CategoryController.Queries.cs
@@ -3,18 +3,8 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace SQLServerForCSharpDevelopers.Controllers.Controllers.Products.V1.Category {
     public partial class CategoryController {
-        [HttpGet("{id}")]
+        [HttpGet("Read")]
         public IActionResult GetCategory(Guid id) {
-            return Ok();
-        }
-
-        [HttpGet("Find/{desc}")]
-        public IActionResult FindCategory(string desc) {
-            return Ok();
-        }
-
-        [HttpGet("Categories")]
-        public IActionResult GetAllCategory() {
             return Ok();
         }
     }
