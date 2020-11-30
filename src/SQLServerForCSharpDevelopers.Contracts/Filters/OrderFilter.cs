c471694b7abb1cc3d07a77518705ee2bbc060dc0
Brian McCarthy <brianmccarthy1@outlook.ie>
2020-11-30 16:34:26 +0000

    bugfix: add missing reference

--- src/SQLServerForCSharpDevelopers.Contracts/Filters/OrderFilter.cs
+++ src/SQLServerForCSharpDevelopers.Contracts/Filters/OrderFilter.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using SQLServerForCSharpDevelopers.Contracts.Sales;
+using SQLServerForCSharpDevelopers.Resources.Models;
 
 namespace SQLServerForCSharpDevelopers.Contracts.Filters {
     public class OrderFilter {
