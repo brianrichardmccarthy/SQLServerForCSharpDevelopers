529f068583796179d348cf3ae1ab374ac43a2c4d
Brian McCarthy <brianmccarthy1@outlook.ie>
2020-11-30 00:08:35 +0000

    feat: added folder structure for Sales controllers, added Address controller

---
+++ SQLServerForCSharpDevelopers.Controllers/Controllers/Sales/V1/Address/AddressController.Queries.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SQLServerForCSharpDevelopers.Controllers.Controllers.Sales.V1.Address {
+    public partial class AddressController {
+        [HttpGet("Read")]
+        public IActionResult ReadAddress() {
+            return Ok();
+        }
+    }
+}
